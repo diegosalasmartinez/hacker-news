e6103ebae20e8b1d8662af55fca44b3cd9c028d1
Diego Salas <diesalasmart@gmail.com>
2022-07-30 23:53:02 -0500

    fix issues and deploy to netlify

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import hackerNews from '../assets/hacker-news.svg'
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <span className="header-logo">HACKER NEWS</span>
+        <span className="header-logo">
+          <img src={hackerNews} alt="Logo" height="30px" width="240px" />
+          by Diego Salas
+        </span>
       </div>
     </div>
   )
